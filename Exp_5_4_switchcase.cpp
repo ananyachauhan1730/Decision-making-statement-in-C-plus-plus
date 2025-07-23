059255131394dc0de7e3b67c605739e465d91009
ananyachauhan1730 <chauhanananya603@gmail.com>
2025-07-23 15:36:03 +0530

    Create Exp_5_4_switchcase.cpp

---
+++ Exp_5_4_switchcase.cpp
@@ -0,0 +1,42 @@
+//Ananya Chauhan 
+//24070123157
+//A3
+#include <iostream>
+using namespace std;
+
+int main(){
+    int choice;
+    cout << "Enter any number from 1 to 5:";
+    cin>>choice;
+    switch(choice){
+        case 1:
+        cout<<"Push_up";
+        break;
+        case 2:
+        cout<<"Bicycle";
+        break;
+        case 3:
+        cout<<"Squat";
+        break;
+        case 4:
+        cout<<"Leg press";
+        break;
+        case 5:
+        cout<<"Weightlifting";
+        break;
+        default:
+        cout<<"Enter wrong choice";
+        break;
+        
+    }
+    return 0;
+}
+
+/*
+output:
+Enter any number from 1 to 5:5
+Weightlifting
+
+Enter any number from 1 to 5:4
+Leg press
+*/  
